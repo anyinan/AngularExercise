fe340bf5fb82058efe4f35f4ab6f2feddc4b035a
Yinan An <yinanan@d207-023-222-130.wireless.sfu.ca>
2017-09-16 16:38:19 -0700

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a>我的</a>
+      <a>联系我们</a>
+      <a>其它</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
